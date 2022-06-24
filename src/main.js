c250b6680e6a2dd1f8f5126b2f61a06cef244877
刘千可 <2319864438@qq.com>
2022-06-24 14:31:23 +0800

    第一天

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+import 'amfe-flexible'
+// import Vant from 'vant';
+// import 'vant/lib/index.css';
 
+// Vue.use(Vant);
 Vue.config.productionTip = false
 
 new Vue({
