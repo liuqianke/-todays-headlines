b2444a50554342e536e2fb09edaeb6bec7ff65a5
刘千可 <2319864438@qq.com>
2022-06-28 08:53:36 +0800

    第二天 我的页面

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,36 @@ const routes = [
     name: 'login',
     component: () => import('@/views/login') //懒加载
   }
+  ,
+  {
+    path: '/',
+    // name: 'layout', // 如果父路由有默认子路由，那它的 name 没有意义
+    component: () => import('@/views/layout'),
+    redirect:"home" ,
+    children: [
+      {
+        path: '/home', // 默认子路由，只能有1个
+        name: 'home',
+        component: () => import('@/views/home')
+      },
+      {
+        path: 'qa',
+        name: 'qa',
+        component: () => import('@/views/qa')
+      },
+      {
+        path: 'video',
+        name: 'video',
+        component: () => import('@/views/video')
+      },
+      {
+        path: 'my',
+        name: 'my',
+        component: () => import('@/views/my')
+      }
+    ]
+  }
+
 ]
 const router = new VueRouter({
   routes
